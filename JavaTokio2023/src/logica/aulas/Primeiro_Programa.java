64c62cf8366ffc18bcdb969f33ef42031b599ce1
logonrmlocal <angellusgabrielneves@gmail.com>
2023-04-13 18:25:24 -0300

    primeiro commit java logica

---
+++ JavaTokio2023/src/logica/aulas/Primeiro_Programa.java
@@ -0,0 +1,25 @@
+package logica.aulas;
+
+public class Primeiro_Programa {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+
+		int idade;
+		idade = 23;
+		System.out.println(idade);
+		
+		idade = 42;
+		System.out.println(+idade);
+		
+		double salario = 1318.20;
+		System.out.println(salario);
+		
+		String nome = "Angellus";
+		System.out.println(nome);
+		
+		System.out.println("Welcome, " + nome + "!");
+		System.out.println(nome + ", você tem " + idade);
+	}
+
+}
---
+++ JavaTokio2023/src/logica/aulas/Segundo_Programa.java
@@ -0,0 +1,17 @@
+package logica.aulas;
+
+import java.util.Scanner;
+
+public class Segundo_Programa {
+
+	public static void main(String[] args) {
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.println("Digite seu nome:");
+		String nome = entrada.nextLine();	
+		System.out.println("Bem-vindo, " + nome);
+		System.out.println("Digite sua idade");
+		System.out.println("Sua idade é: " + entrada.nextInt());
+	}
+}
